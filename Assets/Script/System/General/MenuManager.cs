3211d2bf1afbc1e50534fbe73399f0b1cef45865
hanhercales <deathchan505@gmail.com>
2025-04-09 00:22:22 +0700

    Edit data, fix weapon hitbox

--- Assets/Script/System/General/MenuManager.cs
+++ Assets/Script/System/General/MenuManager.cs
@@ -21,7 +21,25 @@ public class MenuManager : MonoBehaviour
 
     private void Start()
     {
-        if(!PlayerPrefs.HasKey("MainInventorySave" + PlayerPrefs.GetString("PlayerID")))
+        NewSaveID();
+        if(!PlayerPrefs.HasKey("MainInventorySave" + PlayerPrefs.GetString("PlayerID")) || PlayerPrefs.GetString("PlayerID") == "PlayerX00")
             syncButton.interactable = false;
+    }
+
+    private void NewSaveID()
+    {
+        string playerID = PlayerPrefs.GetString("PlayerID");
+        if (playerID != "PlayerX00" && PlayerPrefs.HasKey("MainInventorySavePlayerX00")
+                                    && PlayerPrefs.HasKey("WeaponInventorySavePlayerX00")
+                                    && PlayerPrefs.HasKey("CoinInventorySavePlayerX00"))
+        {
+            PlayerPrefs.SetString("MainInventorySave" + playerID, PlayerPrefs.GetString("MainInventorySavePlayerX00"));
+            PlayerPrefs.SetString("WeaponInventorySave" + playerID, PlayerPrefs.GetString("WeaponInventorySavePlayerX00"));
+            PlayerPrefs.SetString("CoinInventorySave" + playerID, PlayerPrefs.GetString("CoinInventorySavePlayerX00"));
+            
+            PlayerPrefs.DeleteKey("MainInventorySavePlayerX00");
+            PlayerPrefs.DeleteKey("WeaponInventorySavePlayerX00");
+            PlayerPrefs.DeleteKey("CoinInventorySavePlayerX00");
+        }
     }
 }
